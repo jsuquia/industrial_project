3cdec606bd40411bd8ecb2bc5e740c49b7484563
Dan Ward <dan.steve@hotmail.co.uk>
2017-09-18 10:11:47 +0100

    testing chart scripts

---
+++ test.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+
+<head>
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.0/Chart.bundle.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.0/Chart.bundle.min.js"></script>
+
+</head>
+
+<body>
+
+	
+<?php
+include 'testing.php';
+UploadToDatabase('sample.xlsx');
+?>
+
+</body>
+
+</html>
