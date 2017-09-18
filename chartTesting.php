8e45c6dfef2d0f392476f41f0005694d66a32b24
Dan Ward <dan.steve@hotmail.co.uk>
2017-09-18 10:11:23 +0100

    function test

--- chartTesting.php
+++ chartTesting.php
@@ -4,78 +4,37 @@
 <head>
 
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.0/Chart.bundle.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.0/Chart.bundle.min.js"></script>
+	<script src="js_scripts/Chart.js"></script>
+
 
 </head>
 
 <body>
 
 	
-<div style="height:30vh; width:40vw">
+    <div style="height:30vh; width:40vw">
 	<canvas id="myChart"></canvas>
 	</div>
+
+    <div style="height:30vh; width:40vw">
+    <canvas id="myChart2"></canvas>
+    </div>
+
+    <div style="height:30vh; width:40vw">
+    <canvas id="myChart3"></canvas>
+    </div>
+
 <script>
 
-var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-	xAxisID: 'Day',
-    data: {
-        labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
-        datasets: [{
-            label: 'Total Transactions by day',
-            data: [234, 633, 282, 221, 502, 654, 66],
+names = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+dataPoints = [234, 633, 282, 221, 502, 654, 66];
+graphName = "Total Transactions by day";
+barChart(names, dataPoints, graphName, "myChart");
 
-            borderWidth: 1
-        }]
-    }
-});
+names = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+dataPoints = [234, 633, 282, 221, 502, 654, 66];
+graphName = "Total Transactions by day";
+areaChart(names, dataPoints, graphName, "myChart2");
+
 
 </script>
-sdfsdf
-<div style="height:30vh; width:40vw">
-	<canvas id="myChart2""></canvas>
-	</div>
-<script>
-
-var ctx = document.getElementById("myChart2").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
-        datasets: [{
-            label: 'Total Transactions by day',
-            data: [234, 633, 282, 221, 502, 654, 66],
-
-            borderWidth: 1
-        }]
-    }
-});
-
-</script>
-sdfsdf
-<div style="height:30vh; width:40vw">
-	<canvas id="myChart3"></canvas>
-	</div>
-<script>
-
-var ctx = document.getElementById("myChart3").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
-        datasets: [{
-            label: 'Total Transactions by day',
-            data: [234, 633, 282, 221, 502, 654, 66],
-
-            borderWidth: 1
-        }]
-    }
-});
-
-</script>
-sdfsdf
-
-</body>
-
-</html>
